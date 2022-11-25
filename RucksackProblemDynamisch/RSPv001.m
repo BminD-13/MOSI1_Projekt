0b9c3916fca3049448fde68ed35e0725c6ff63aa
BminD-13 <benjamind1995@googlemail.com>
2022-11-25 22:49:43 +0100

    daten erstellt

---
+++ RucksackProblemDynamisch/RSPv001.m
@@ -0,0 +1,17 @@
+clear
+close all
+
+%% Data
+
+
+% knapsack data
+knapsackCapacity = 10;
+
+% item data
+Capacity = [3,8,2,5,4,9,7,4,8,1]';
+Value = [7,4,8,4,5,3,6,7,5,6]';
+
+item = table(Capacity, Value);
+
+
+%
