94b58c0132fa669b853c8279e46c5baba26b0d2a
BminD-13 <benjamind1995@googlemail.com>
2022-11-26 18:53:23 +0100

    algorithmus funktionsfähig und an einem minimalbeispiel getestet

--- RucksackProblemDynamisch/RSPv001.m
+++ RucksackProblemDynamisch/RSPv001.m
@@ -14,27 +14,51 @@ item = table(Capacity, Value);
 
 [a,b,c] = knapsackDynamic(knapsackCapacity, item);
 
-% algorithmus
+%% knapsack dynamic algorithm
 function [valueMax, sumTable, itemsPicked] = knapsackDynamic(knapsackCapacity, item)
-itemsPicked=0;
+
+%% declaration
+itemCount = length(item.Capacity);
+itemsPicked = [];
 sumTable = zeros(length(item.Capacity) + 1,knapsackCapacity);
 
-% filling the table with the information 
-for cntItem = 1:length(item.Capacity)
-    for cntCap = 1:knapsackCapacity+1        
-        if cntCap <= item.Capacity(cntItem)            
-            valueAbove = sumTable(cntItem, cntCap);
+%% filling the knapsack table with the information 
 
-            sumTable(cntItem + 1, cntCap) = valueAbove;
+% iterate rows (items) of the table
+    for cntItem = 1:itemCount
 
-        else            
-            valueItem = item.Value(cntItem);
-            valueDiagonal = sumTable(cntItem, cntCap-item.Capacity(cntItem));
+    % iterate columns (knapsackCapacity) of the table
+        for cntCap = 1:knapsackCapacity+1        
 
-            sumTable(cntItem + 1, cntCap) = valueItem + valueDiagonal;
+        % knapsack Capacity smaller than item capacity
+            if cntCap <= item.Capacity(cntItem)            
+                valueAbove = sumTable(cntItem, cntCap);
+    
+                sumTable(cntItem + 1, cntCap) = valueAbove;
 
+        % knapsack Capacity bigger than item capacity
+            else            
+                valueItem = item.Value(cntItem);
+                valueDiagonal = sumTable(cntItem, cntCap-item.Capacity(cntItem));
+    
+                sumTable(cntItem + 1, cntCap) = valueItem + valueDiagonal;    
+            end
         end
     end
+
+valueMax = sumTable(length(item.Capacity) + 1,knapsackCapacity+1);
+
+
+%% choosing most suitable items by evaluating the table
+columnActual = knapsackCapacity + 1;
+items = linspace(itemCount+1,2,itemCount);
+for rowActual = items
+    rowAbove = rowActual-1;
+
+    if sumTable(rowActual, columnActual) > sumTable(rowAbove, columnActual)
+        itemsPicked(end+1) = rowActual-1;
+        columnActual = columnActual - item.Capacity(rowActual-1);
+    end
 end
-valueMax = sumTable(length(item.Capacity) + 1,knapsackCapacity);
+
 end
