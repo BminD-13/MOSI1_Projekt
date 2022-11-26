75f50b7c1d214816d45d1e3bf02d540ca496d53c
BminD-13 <benjamind1995@googlemail.com>
2022-11-26 09:47:41 +0100

    fuellen der Tabelle funktioniert an kleinem Beispiel

--- RucksackProblemDynamisch/RSPv001.m
+++ RucksackProblemDynamisch/RSPv001.m
@@ -1,17 +1,40 @@
 clear
 close all
-
+clc
 %% Data
 
-
 % knapsack data
-knapsackCapacity = 10;
+knapsackCapacity = 7;
 
 % item data
-Capacity = [3,8,2,5,4,9,7,4,8,1]';
-Value = [7,4,8,4,5,3,6,7,5,6]';
+Capacity = [3,1,3,4,2]';
+Value = [2,2,4,5,3]';
 
 item = table(Capacity, Value);
 
+[a,b,c] = knapsackDynamic(knapsackCapacity, item);
 
-%
+% algorithmus
+function [valueMax, sumTable, itemsPicked] = knapsackDynamic(knapsackCapacity, item)
+itemsPicked=0;
+sumTable = zeros(length(item.Capacity) + 1,knapsackCapacity);
+
+% filling the table with the information 
+for cntItem = 1:length(item.Capacity)
+    for cntCap = 1:knapsackCapacity+1        
+        if cntCap <= item.Capacity(cntItem)            
+            valueAbove = sumTable(cntItem, cntCap);
+
+            sumTable(cntItem + 1, cntCap) = valueAbove;
+
+        else            
+            valueItem = item.Value(cntItem);
+            valueDiagonal = sumTable(cntItem, cntCap-item.Capacity(cntItem));
+
+            sumTable(cntItem + 1, cntCap) = valueItem + valueDiagonal;
+
+        end
+    end
+end
+valueMax = sumTable(length(item.Capacity) + 1,knapsackCapacity);
+end
